f914d273a5533e46429b8d086ec7dc20904df385
C. Regouby <christophe.regouby@free.fr>
2023-09-25 00:11:35 +0200

    initiate model

---
+++ R/layoutlmv3.R
@@ -0,0 +1,9 @@
+# References:
+# - https://github.com/microsoft/unilm/tree/master/layoutlmv3
+# - https://github.com/huggingface/transformers/blob/main/src/transformers/models/layoutlmv3/modeling_layoutlmv3.py
+
+#' @noRd
+#' @importFrom zeallot %<-%
+#' @importFrom purrr map
+#' @import torch
+NULL
